64e7ff377d5e878cf1b95962d359959dfce46d2a
lishuo <lishuocoder@163.com>
2020-03-03 13:34:34 +0800

    init my object

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>这是helloworld组件</h2>
+  </div>
+</template>
+
+<script>
+import { Toast } from "mint-ui";
+
+export default {
+  name: "HelloWorld",
+  data() {
+    return {};
+  },
+  methods: {
+  
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+//引入mint-ui
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+//引入mui
+import './lib/mui/css/mui.min.css'
+
+
+Vue.config.productionTip = false
+Vue.use(MintUI)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
